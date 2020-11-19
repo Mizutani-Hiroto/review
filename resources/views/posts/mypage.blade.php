97bd677617dc6bd1978a1b488f9099c66ba13c2c
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-11-19 14:51:42 +0000

    add username

--- resources/views/posts/mypage.blade.php
+++ resources/views/posts/mypage.blade.php
@@ -17,6 +17,7 @@
   <div class="card">
     <div class="card-body">
       <h5></h5>
+      <p>{{ $post->user->name  }}</p>
       <h5 class="card-title">{{ $post->title }}</h5>
         <p class="card-text">{{ $post->content }}</p>
 
