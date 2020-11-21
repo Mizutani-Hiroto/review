62809b6ec88a72c54032e53479214e01949979b4
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-11-21 11:55:13 +0000

    add postseeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            [
+                'title' => 'seederの管理',
+                'content' => 'seederを使うことにより初期値を入れることができる',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+                        ],
+                        [
+                            'title' => 'seederの管理',
+                            'content' => 'PostをUserと関連付けしないといけないのではないか',
+                            'created_at' => date('Y-m-d H:i:s'),
+                            'updated_at' => date('Y-m-d H:i:s'),
+                            ],
+                            [
+                                'title' => 'seederの管理',
+                                'content' => 'seederを設定しておくと初期値にしても消えません',
+                                'created_at' => date('Y-m-d H:i:s'),
+                                'updated_at' => date('Y-m-d H:i:s'),
+                                ],
+                                [
+                                    'title' => 'seederの管理',
+                                    'content' => 'idを入れるべきがどうか悩む',
+                                    'created_at' => date('Y-m-d H:i:s'),
+                                    'updated_at' => date('Y-m-d H:i:s'),
+                                    ]
+                    ]);
+    }
+}
