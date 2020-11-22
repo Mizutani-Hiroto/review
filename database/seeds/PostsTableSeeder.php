77e77bda22fa0f2dee470a6d42ec0bbc60c2be26
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-11-22 14:57:46 +0000

    edit seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -14,24 +14,28 @@ class PostsTableSeeder extends Seeder
     {
         DB::table('posts')->insert([
             [
+                'id' => '2',
                 'title' => 'seederの管理',
                 'content' => 'seederを使うことにより初期値を入れることができる',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
                         ],
                         [
+                            'id' => '2',
                             'title' => 'seederの管理',
                             'content' => 'PostをUserと関連付けしないといけないのではないか',
                             'created_at' => date('Y-m-d H:i:s'),
                             'updated_at' => date('Y-m-d H:i:s'),
                             ],
                             [
+                                'id' => '3',
                                 'title' => 'seederの管理',
                                 'content' => 'seederを設定しておくと初期値にしても消えません',
                                 'created_at' => date('Y-m-d H:i:s'),
                                 'updated_at' => date('Y-m-d H:i:s'),
                                 ],
                                 [
+                                    'id' => '3',
                                     'title' => 'seederの管理',
                                     'content' => 'idを入れるべきがどうか悩む',
                                     'created_at' => date('Y-m-d H:i:s'),
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,23 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            [
-                'name' => '田中由紀子',
-                'email' => 'abc@au.com',
-                'email_verified_at' => 'null',
-                'password' => 'abc123',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s')
-                ],
-                [
-                'name' => '鈴木翔太',
-                'email' => 'def@au.com',
-                'email_verified_at' => 'null',
-                'password' => 'def456',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s')
-                ]
-            ]);
+        $users = factory(App\User::class,6)->create();
     }
 }
