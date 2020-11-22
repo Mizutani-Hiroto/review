15bc73e6f46c0a1331e707e4387ec829fa52ca4a
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-11-22 15:55:24 +0000

    create Postseeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'user_id' => function(){
+            return factory(App\User::class)->create()->id;
+        },
+        'title' => $faker->sentence(rand(1,4)),
+        'content' => $faker->realText(512),
+        'created_at' => date('Y-m-d H:i:s')
+    ];
+});
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,35 +12,6 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            [
-                'id' => '2',
-                'title' => 'seederの管理',
-                'content' => 'seederを使うことにより初期値を入れることができる',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-                        ],
-                        [
-                            'id' => '2',
-                            'title' => 'seederの管理',
-                            'content' => 'PostをUserと関連付けしないといけないのではないか',
-                            'created_at' => date('Y-m-d H:i:s'),
-                            'updated_at' => date('Y-m-d H:i:s'),
-                            ],
-                            [
-                                'id' => '3',
-                                'title' => 'seederの管理',
-                                'content' => 'seederを設定しておくと初期値にしても消えません',
-                                'created_at' => date('Y-m-d H:i:s'),
-                                'updated_at' => date('Y-m-d H:i:s'),
-                                ],
-                                [
-                                    'id' => '3',
-                                    'title' => 'seederの管理',
-                                    'content' => 'idを入れるべきがどうか悩む',
-                                    'created_at' => date('Y-m-d H:i:s'),
-                                    'updated_at' => date('Y-m-d H:i:s'),
-                                    ]
-                    ]);
+        $posts = factory(App\Post::class,6)->create();
     }
 }
